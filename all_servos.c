311b36d9d1fb9138f695b625e789977a4561bd56
Plainview Robotics <46660273+JonathanHarris15@users.noreply.github.com>
2022-03-07 10:25:17 -0600

    Update all_servos.c
    Fixed the code to a "working state". Still dumpster fire garbage and will probably not fix later but I can dream right?

--- all_servos.c
+++ all_servos.c
@@ -1,14 +1,34 @@
-void all_servos(int targetpositions[]){
-    bool not_done = true;
-    while(not_done){
-        not_done = false;
-        for(int i = 0; i < 4; i ++){
-            int current_pos = get_servo_position(i);
-            int change = (targetpositions[i] - get_servo_position(i)) * 0.4;
-            set_servo_position(i, current_pos + change);
-            msleep(5)
-            if(get_servo_position(i) != targetpositions[i]){
-                not_done = true;
+
+int positions[] = {-1,-1,-1,-1};
+void all_servos(int a, int b, int c, int d){
+    positions[0] = a;
+    positions[1] = b;
+    positions[2] = c;
+    positions[3] = d;
+    servos(positions);
+}
+void servos(int targetpositions[]){
+    int not_done = 0;
+    while(not_done == 0){
+        not_done = 1;
+        int i;
+        for(i = 0; i < 4; i ++){
+            if(targetpositions[i] > -1){    
+                int current_pos = get_servo_position(i);
+                if(current_pos > targetpositions[i] + 3 || current_pos < targetpositions[i] - 3){
+                    printf("hello\n");
+                    not_done = 0;
+                    if(current_pos > targetpositions[i]){
+                        printf("going down: %d\n",i);
+                        set_servo_position(i,current_pos - 5);
+                        msleep(5);
+                    }
+                    if(current_pos < targetpositions[i]){
+                        printf("going up: %d\n", i);
+                        set_servo_position(i,current_pos + 5);
+                        msleep(5);
+                    }
+                }
             }
         }
     }
